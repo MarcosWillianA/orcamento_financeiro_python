e8ec46d06d09fec4e8b493359d25c1b20ee34551
Marcos Willian <marcoswillianl1987@gmail.com>
2026-06-09 14:02:57 -0400

    registrar_saida
    Lógica do registro de saídas feito

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ while rodando:
         tipo_movimentacao = 'Entrada'
         
         while True:
-            descricao_movimentacao = input('Que tipo de entrada quer registrar? ').strip()
+            entrada_movimentacao = input('Que tipo de entrada quer registrar? ').strip()
+            descricao_movimentacao = entrada_movimentacao.capitalize()
             if descricao_movimentacao != '':
                 break
             else:
@@ -48,6 +49,31 @@ while rodando:
     
         registrar_movimentacoes(tipo_movimentacao, descricao_movimentacao, valor_movimentacao)
         print(f'Entrada de {descricao_movimentacao} de R${valor_movimentacao:.2f} registrada com sucesso \n')
+        
+    elif opcao_usuario == '2':
+        tipo_movimentacao = 'Saída'
+        
+        while True:
+            saida_movimentacao = input('Que tipo de saída quer registrar? ').strip()
+            descricao_movimentacao = saida_movimentacao.capitalize()
+            if descricao_movimentacao != '':
+                break
+            else:
+                print('A descrição não pode ficar em branco, por favor digite uma descrição adequada')
+        while True:
+            saida_usuario = input('Que tipo de valor quer registrar? ').strip()
+            saida_formatada = saida_usuario.replace(',','.')
+            if saida_formatada.replace('.', '', 1).isdigit():
+                valor_movimentacao = round(float(saida_formatada), 2)
+                if valor_movimentacao > 0:
+                    break
+                else:
+                    print('Erro: o valor de saída deve ser maior que zero')
+            else:
+                print('Erro: digite um valor válido (Ex.: 165,00 ou 165.00 ou 165).')
+        
+        registrar_movimentacoes(tipo_movimentacao, descricao_movimentacao, valor_movimentacao)
+        print(f'Saída de {descricao_movimentacao} de R${valor_movimentacao:.2f} registrada com sucesso \n')
             
     elif opcao_usuario == '5':
         print('Volte sempre!')
